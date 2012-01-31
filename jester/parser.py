dfb5e5173c7a9dcf109b4e63e9f840f05114538d
Jonathan Haddad <jon@jonhaddad.com>
2012-01-31 15:17:04 -0800

    new parser rules for create rule rulename

--- jester/parser.py
+++ jester/parser.py
@@ -1,7 +1,8 @@
 
 
 from pyparsing import Word, alphas, nums, alphanums, \
-        Keyword, LineEnd, Optional, oneOf, LineStart
+        Keyword, LineEnd, Optional, oneOf, LineStart, \
+        Combine
 
 class Parser(object):
 
@@ -13,6 +14,15 @@ class Parser(object):
     in_ = Keyword('in', caseless=True)
     to = Keyword('to', caseless=True)
     userid = Word(alphanums + "-_")
+    show = Keyword('show', caseless=True)
+    rules = Keyword('rules', caseless=True)
+    rule = Keyword('rule', caseless=True)
+    create = Keyword('create', caseless=True)
+    rule_name = Word(alphanums + "_-")
+
+    create_rule = create + rule
+    create_rule.setResultsName('create_rule')
+    create_rule_name = create_rule + rule_name
 
     # keywords - predicates
     when = Keyword('when', caseless=True)
@@ -34,13 +44,17 @@ class Parser(object):
 
     on_event = on_ + award_name.setResultsName('on_event') 
 
-    rule = on_event + award + points_or_badge \
+    rule = create_rule_name +  on_event + award + points_or_badge \
                 + Optional(predicate)
     # raw award
     raw_award = award + points_or_badge + to.suppress() + userid('userid')
+
+    # shw rules
+    show_rules = Combine( show + rules )('show_rules')
+
     ## final
     command = LineStart() + \
-              (rule ^ raw_award) + \
+              (rule | raw_award | show_rules) + \
               LineEnd()
 
     @classmethod
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -5,10 +5,13 @@ from jester.parser import Parser
 
 class ParserTest(LogTrapTestCase):
 
+    def test_create_rule_partial(self):
+        tmp = Parser.create_rule.parseString('create rule')
+
     def test_rules(self):
-        t = ['on game_play award 5 points',
-             'on gold_miner award badge suckit',
-             'on eat_bacon award badge fatass']
+        t = ['create rule blah on game_play award 5 points',
+             'create rule blah2 on gold_miner award badge suckit',
+             'create rule blah3 on eat_bacon award badge fatass']
 
         for s in t:
             tree = Parser.parse( s )
@@ -17,12 +20,12 @@ class ParserTest(LogTrapTestCase):
 
     def test_partial_predicates(self):
         s = "when play occurs 3 times in 1 day"
-        Parser.predicate(s)
+        result = Parser.predicate.parseString(s)
 
     def test_predicates(self):
-        t = ['on game_play award badge big_player when game_play occurs 3 times in 1 day',
-             'on game_play award 6 points  when game_play occurs 3 times in 1 day',
-             'on kick_tina_in_face award badge face_kicker when kick_tina_in_face occurs 10 times in 1 year']
+        t = ['create rule big_player on game_play award badge big_player when game_play occurs 3 times in 1 day',
+             'create rule game_play_points on game_play award 6 points  when game_play occurs 3 times in 1 day',
+             'create rule kick_tina_face on kick_tina_in_face award badge face_kicker when kick_tina_in_face occurs 10 times in 1 year']
         for s in t:
             tree = Parser.parse( s )
 
@@ -30,3 +33,7 @@ class ParserTest(LogTrapTestCase):
         Parser.parse("award 90 points to 105")
         tree = Parser.parse("award 90 points to jhaddad")
         tree = Parser.parse("award badge bacon_lover to jhaddad")
+
+    def test_show_rules(self):
+        return
+        Parser.parse('show rules')
