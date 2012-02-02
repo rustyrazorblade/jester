f50e433685c0263ceec706ceac8b9ed8bdce397c
Jonathan Haddad <jon@jonhaddad.com>
2012-02-01 22:57:49 -0800

    #4 setup bin/run to actually work - adding it to supervisor next

--- jester/server.py
+++ jester/server.py
@@ -3,9 +3,11 @@ from tornado.netutil import TCPServer
 from tornado.ioloop import IOLoop
 #from tornado.iostream import IOStream
 import json
+from logging import info
 
 class JesterServer(TCPServer):
     def handle_stream(self, stream, address):
+        info("incoming connection")
         Connection(stream, address)
 
 class Connection(object):
@@ -24,8 +26,8 @@ class Connection(object):
         self.stream.read_until('\n', self.read_line)
 
 
-
-server = JesterServer()
-server.listen(8888)
-IOLoop.instance().start()
+def run():
+    server = JesterServer()
+    server.listen(8888)
+    IOLoop.instance().start()
 
